005cbcabf3ff5baf5750d935f00ea5429df31bc8
Najril19 <najrilmuhammad02@gmail.com>
2026-04-13 23:16:25 +0700

    Remove backend folder - converted to Next.js API routes

--- backend/src/database/init.ts
+++
@@ -1,6 +0,0 @@
-import { initializeDatabase } from './schema';
-import path from 'path';
-
-const dbPath = path.join(__dirname, '../../database.sqlite');
-initializeDatabase(dbPath);
-console.log('Database initialization complete!');
